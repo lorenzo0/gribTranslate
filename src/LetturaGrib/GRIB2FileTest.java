445c6e8ad7512fd920b63441f3a00061ef258af3
lorenzo0 <lorenzopisa00@gmail.com>
2019-04-23 09:43:37 +0200

    Versione funzionante. Sample: 44.3, 10,3

---
+++ src/LetturaGrib/GRIB2FileTest.java
@@ -0,0 +1,73 @@
+//package com.ph.grib2tools.test;
+
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import com.ph.grib2tools.grib2file.GribFile;
+import com.ph.grib2tools.grib2file.GribSection1;
+import com.ph.grib2tools.grib2file.RandomAccessGribFile;
+import com.ph.grib2tools.grib2file.griddefinition.GridDefinitionTemplate30;
+import com.ph.grib2tools.grib2file.productdefinition.ProductDefinitionTemplate40;
+import com.ph.grib2tools.grib2file.productdefinition.ProductDefinitionTemplate4x;
+
+public class GRIB2FileTest {
+
+	public static void main(String[] args) {
+		
+		/*if (args.length < 1) {
+			System.out.println("Syntax: java GRIB2FileTest <filename> <structureid (optional)>");
+			return;
+		}*/
+
+		// Name of the GRIB2 file
+		String filename = "format.grib";
+		
+		// Defines how many GRIB file structures shall be skipped when reading the GRIB2 file. This
+		// is useful since some organizations put several GRIB2 file structures in one file.
+		int numskip;
+		try {
+			numskip = Integer.parseInt(args[1]);
+		} catch (Exception e) {
+			numskip = 0;
+		}
+				
+		// Id of the grid within the GRIBF2 file, since GRIB2 files can contain several grids (Sections 5-7)
+		int gridid = 0;
+		
+		System.out.println("Reading file " + filename + ", file structure " + numskip + ":");
+		try {
+
+			// Open GRIB2 file and read it
+			InputStream inputstream;
+			inputstream = Files.newInputStream(Paths.get(filename));
+			RandomAccessGribFile gribFile = new RandomAccessGribFile("testdata", filename);
+			gribFile.importFromStream(inputstream, numskip);
+			
+			// Get identification information
+			GribSection1 section1 = gribFile.getSection1();
+			System.out.println("Date: " + String.format("%02d", section1.day) + "." + String.format("%02d", section1.month) + "." + section1.year);
+			System.out.println("Time: " + String.format("%02d", section1.hour) + ":" + String.format("%02d", section1.minute) + "." + String.format("%02d", section1.second));
+			System.out.println("Generating centre: " + section1.generatingCentre);
+			// Get product information
+			ProductDefinitionTemplate4x productDefinition = (ProductDefinitionTemplate4x)gribFile.getProductDefinitionTemplate();
+			/*System.out.println("Forecast time: " + productDefinition.forecastTime);
+			System.out.println("Parameter category: " + productDefinition.parameterCategory);
+			System.out.println("Parameter number: " + productDefinition.parameterNumber);*/
+
+			// Get grid information
+			GridDefinitionTemplate30 gridDefinition = (GridDefinitionTemplate30)gribFile.getGridDefinitionTemplate();
+			System.out.println("Covered area:");
+			System.out.println("   from (latitude, longitude): " + GribFile.unitsToDeg(gridDefinition.firstPointLat) + ", " + GribFile.unitsToDeg(gridDefinition.firstPointLon));		
+			System.out.println("   to: (latitude, longitude): " + GribFile.unitsToDeg(gridDefinition.lastPointLat) + ", " + GribFile.unitsToDeg(gridDefinition.lastPointLon));		
+
+			// Get grid data
+			double latitude = 44.3;
+			double longitude = 10.3;
+			System.out.println("Value at (" + latitude + ", " + longitude + "): " + gribFile.interpolateValueAtLocation(gridid, latitude, longitude));
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/LetturaGrib/Grib2Input.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LetturaGrib;
+import com.ph.grib2tools.grib2file.GribFile;
+import java.util.*;
+import java.io.*;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+//import GRIB2Tools-master.*;
+
+/**
+ *
+ * @author loren
+ */
+public final class Grib2Input extends java.lang.Object{
+    /*
+    Grib2Input grib2Input;
+    String pathfile = "format.grib";
+    
+    GribFile input;
+    
+    InputStream is;
+    
+    String fileName = "src/resources/thermopylae.txt";
+
+        try (InputStream fis = new FileInputStream(fileName);
+                InputStreamReader isr = new InputStreamReader(fis,
+                        StandardCharsets.UTF_8);
+                BufferedReader br = new BufferedReader(isr)) {
+
+            br.lines().forEach(line -> System.out.println(line));
+        }
+    
+
+    //Create RandomAccessFile
+    RandomAccessFile gribfile = null;
+    
+    try {
+      gribfile = new ucar.unidata.io.RandomAccessFile(pathfile, "rw");
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    //Create grib2Input file
+    Grib2Input file = new Grib2Input();      
+    try {
+      grib2Input.scan(false,  false);       
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+*/
+}
